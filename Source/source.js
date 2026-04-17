114f5f47f9cdda6bac2f3d279cbfcf1852126310
XxAbdo <abdulrahmanmohamed420@gmail.com>
2026-04-17 21:46:35 +0300

    added the checkout page

--- Source/source.js
+++ Source/source.js
@@ -315,12 +315,16 @@ function checkAuthState() {
         
 
         let navNameElement = document.querySelector('.nav_username');
-        let navPfp = document.querySelector('.pfp img');
+        let navPfp = document.querySelector('.pfp');
         
         if (navNameElement) navNameElement.textContent = currentUser;
         
         if (currentUser !== 'Eren_yeager') {
-           navPfp.src = '../Images/website_images/website_logo.png';
+           navPfp.innerHTML = `
+                                <svg class="account-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                                </svg>`
         }
 
 
